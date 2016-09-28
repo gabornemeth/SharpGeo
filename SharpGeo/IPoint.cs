47811df76abf819d0d1e3e1ec7a96df25d89d786
Németh Gábor <gabor.nemeth.dev@gmail.com>
2016-09-28 09:51:31 +0200

    rename to SharpGeo

---
+++ SharpGeo/IPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGeo
+{
+    /// <summary>
+    /// Interface of a point. Both 2D and 3D point-like objects should implement
+    /// </summary>
+    public interface IPoint
+    {
+        float X { get; set; }
+        float Y { get; set; }
+        bool IsEmpty { get; }
+    }
+}
